a7c73af052fb6452e2fa63d45b7fd200ea5db5f5
Fatima <fat.tka4enko2014@yandex.ru>
2026-09-08 15:05:09 +0300

    Upgrade to Java 17 and Selenide 7.3.3

--- src/test/java/ru/netology/ibank/page/LoginPage.java
+++ src/test/java/ru/netology/ibank/page/LoginPage.java
@@ -1,11 +1,9 @@
 package ru.netology.ibank.page;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -20,15 +18,7 @@ public class LoginPage {
         loginField.setValue(login);
         passwordField.setValue(password);
         loginButton.click();
-
-        // Ждём появления страницы подтверждения
-        Selenide.sleep(5000);
-        verificationField.should(visible, Duration.ofSeconds(15)).click();
-        verificationField.setValue(verificationCode);
-
-        // Убеждаемся, что значение введено
-        verificationField.shouldHave(value(verificationCode), Duration.ofSeconds(5));
-
+        verificationField.should(visible, Duration.ofSeconds(10)).setValue(verificationCode);
         verifyButton.click();
     }
 }
